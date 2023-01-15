9626c6ca0296410abe7d2f8c06d09555c7fc608b
odulaja philip <odulajaphilip@gmail.com>
2023-01-15 15:53:46 +0100

    created product urls pattern

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, response
 from .serializers import ProductSerializer
+from .models import Products
 
 # Create your views here.
 class ProductViewSet(viewsets.ViewSet):
-    def list(self, request): # api/products
+    def list(self, request): # /api/products
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        if serializer.is_valid:
+            response.  Response(serializer.data)
+
+    def create(self, request): # /api/products
         pass
 
-    def create(self, request): #
+    def retrieve(self, request, pk=None): # /api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None) : # /api/products/<str:id>
+        pass
+
+    def destroy(self, request, pk=None) : # /api/products/<str:id>
+        pass
