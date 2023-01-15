675f8b4466a8502b8a02a3264e3227260423b6db
odulaja philip <odulajaphilip@gmail.com>
2023-01-15 11:42:10 +0100

    created product serializer

--- admin/products/models.py
+++ admin/products/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Products(models.Model):
-    title = models.CharField(max_length=280)
-    image = models.CharField(max_length=280)
-    image = models.PositiveIntegerField(default=0)
+    title= models.CharField(max_length=280)
+    image= models.CharField(max_length=280)
+    likes= models.PositiveIntegerField(default=0)
 
-class User(models.Model):
+
+class USer(models.Model):
     pass
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): # api/products
+        pass
+
+    def create(self, request): #
